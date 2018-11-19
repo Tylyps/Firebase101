3cd9dba8bcbb09c453a07f1fb90c67828c223e35
Tylyps <j.chmielewski@o2.pl>
2018-11-19 07:32:33 +0100

    Update data in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,6 +17,11 @@ database.ref().set({
   name: 'Jakub Chmielewski',
   age: 21,
   isSingle: false,
+  stressLevel: 6,
+  job: {
+    title: 'Software developer',
+    company: 'Bitcraft'
+  },
   location: {
     city: 'Białystok',
     country: 'Poland'
@@ -25,7 +30,21 @@ database.ref().set({
   console.log('Data is saved');
 }).catch((error) => {
   console.log('This faled.', e);
-})
+});
+
+// database.ref().update({
+//   name: 'Kuba',
+//   age: 20,
+//   job: 'Software developer',
+//   isSingle: null,
+//   'location/city': 'Warszawa'
+// });
+
+database.ref().update({
+  stressLevel: 9,
+  'job/company': 'Netguru',
+  'location/city': 'Poznań'
+});
 
 // database.ref().set('This is my data');
 // database.ref('age').set(27);
@@ -35,11 +54,9 @@ database.ref().set({
 // }).catch((e) => {
 //   console.log('Things didnt for the second error', e);
 // });
-
-database.ref('isSingle').remove().then(() => {
-    console.log('Data was removed.');
-  }).catch((e) => {
-    console.log('Did not remove data', e);
-  });
-
-database.ref('age').set(null);
+// database.ref('isSingle').remove().then(() => {
+//     console.log('Data was removed.');
+//   }).catch((e) => {
+//     console.log('Did not remove data', e);
+//   });
+// database.ref('age').set(null);
